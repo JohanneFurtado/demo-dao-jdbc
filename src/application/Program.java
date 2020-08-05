5b348a1bb45a5ab747f30c70799bfaf2cb6ed0c1
cedricapollon <gatuno0609@gmail.com>
2020-08-05 03:45:39 +0100

    Insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,17 +2,22 @@ package application;
 
 import java.util.Date;
 
+import model.dao.Daofactory;
+import model.dao.SellerDao;
+import model.dao.impl.SellerDaoJDBC;
 import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
+		SellerDao sellerDao = Daofactory.createSellerDao();
 		
-		Department obj = new Department(1, "Books");
+		Department obj = new Department(1, null);
 		
-		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
-		System.out.println(seller);
+		Seller seller = new Seller(null, "Denis", "denis@gmail.com", new Date(), 3000.0, obj);
+		sellerDao.insert(seller);
+		System.out.println("Inserted! New id = " + seller.getId());
 
 	}
 }
